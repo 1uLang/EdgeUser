bad91070d74b6e7e0591388a61571c35dd348ff3
刘祥超 <iwind.liu@gmail.com>
2021-01-31 16:04:06 +0800

    优化js组件

--- web/public/js/components/common/checkbox.js
+++ web/public/js/components/common/checkbox.js
@@ -24,6 +24,13 @@ Vue.component("checkbox", {
 			this.$emit("input", this.newValue)
 		}
 	},
+    watch: {
+        value: function (v) {
+            if (typeof v == "boolean") {
+                this.newValue = v
+            }
+        }
+    },
 	template: `<div class="ui checkbox">
 	<input type="checkbox" :name="name" :value="elementValue" :id="elementId" @change="change" v-model="newValue"/>
 	<label :for="elementId"><slot></slot></label>
