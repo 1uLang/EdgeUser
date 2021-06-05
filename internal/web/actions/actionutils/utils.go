c1e45c2cf925eadf29bf2b4c5e4068ecbb8f93ff
刘祥超 <iwind.liu@gmail.com>
2021-06-05 09:39:52 +0800

    优化错误提示

--- internal/web/actions/actionutils/utils.go
+++ internal/web/actions/actionutils/utils.go
@@ -5,6 +5,7 @@ import (
 	teaconst "github.com/TeaOSLab/EdgeUser/internal/const"
 	"github.com/iwind/TeaGo/actions"
 	"github.com/iwind/TeaGo/logs"
+	"net/http"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -12,28 +13,44 @@ import (
 	"strings"
 )
 
-// 提示服务器错误信息
+// Fail 提示服务器错误信息
 func Fail(action actions.ActionWrapper, err error) {
 	if err != nil {
 		logs.Println("[" + reflect.TypeOf(action).String() + "]" + findStack(err.Error()))
 	}
-	action.Object().Fail(teaconst.ErrServer)
+	action.Object().Fail(teaconst.ErrServer + "（" + err.Error() + "）")
 }
 
-// 提示页面错误信息
+// FailPage 提示页面错误信息
 func FailPage(action actions.ActionWrapper, err error) {
 	if err != nil {
 		logs.Println("[" + reflect.TypeOf(action).String() + "]" + findStack(err.Error()))
 	}
-	action.Object().WriteString(teaconst.ErrServer)
+	action.Object().ResponseWriter.WriteHeader(http.StatusInternalServerError)
+	if len(action.Object().Request.Header.Get("X-Requested-With")) > 0 {
+		action.Object().WriteString(teaconst.ErrServer)
+	} else {
+		action.Object().WriteString(`<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+	<div style="background: #eee; border: 1px #ccc solid; padding: 10px; font-size: 12px; line-height: 1.8">
+	` + teaconst.ErrServer + `
+	<div>可以通过查看 <strong><em>$安装目录/logs/run.log</em></strong> 日志文件查看具体的错误提示。</div>
+	<hr style="border-top: 1px #ccc solid"/>
+	<div style="color: red">Error: ` + err.Error() + `</pre>
+	</div>
+	</body>
+</html>`)
+	}
 }
 
-// 判断动作的文件路径是否相当
+// MatchPath 判断动作的文件路径是否相当
 func MatchPath(action *actions.ActionObject, path string) bool {
 	return action.Request.URL.Path == path
 }
 
-// 查找父级Action
+// FindParentAction 查找父级Action
 func FindParentAction(actionPtr actions.ActionWrapper) *ParentAction {
 	parentActionValue := reflect.ValueOf(actionPtr).Elem().FieldByName("ParentAction")
 	if parentActionValue.IsValid() {
