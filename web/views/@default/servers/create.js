2c455cdd238d1e0315aa95616302bc4d3997fae4
刘祥超 <iwind.liu@gmail.com>
2021-06-17 21:12:00 +0800

    网站主机名默认跟随CDN

--- web/views/@default/servers/create.js
+++ web/views/@default/servers/create.js
@@ -1,6 +1,6 @@
 Tea.context(function () {
 	this.supportHTTPS = false
-	this.requestHostType = 1
+	this.requestHostType = 0
 	this.requestHost = ""
 
 	this.success = NotifySuccess("保存成功", "/servers")
