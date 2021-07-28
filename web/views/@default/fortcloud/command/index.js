ca8a27bf5fded713453a01344cfed4747d3faeaa
1uLang <243971996@qq.com>
2021-07-28 17:17:51 +0800

    jumpserver 组件

---
+++ web/views/@default/fortcloud/command/index.js
@@ -0,0 +1,45 @@
+Tea.context(function () {
+
+    this.$delay(function () {
+
+        teaweb.datepicker("day-from-picker")
+        teaweb.datepicker("day-to-picker")
+
+    })
+
+    this.onChangeCheckTime = function (index) {
+
+        if (this.index != index) {
+            this.index = index
+        }
+        this.onSearch()
+    }
+
+    this.onSearch = function () {
+        window.location = "/fortcloud/command?index=" + this.index +
+            "&asset=" + this.asset + "&input=" + this.input + "&username=" +
+            this.username + "&riskLevel=" + this.riskLevel + "&dayFrom=" +
+            this.dayFrom + "&dayTo=" + this.dayTo
+    }
+    this.onRefresh = function () {
+        window.location = "/fortcloud/command"
+    }
+    this.onTimeChange = function () {
+
+        let startTime = document.getElementById("day-from-picker").value
+        let endTime = document.getElementById("day-to-picker").value
+
+        if(this.dayFrom != startTime || this.dayTo != endTime) {
+            this.dayFrom = startTime
+            this.dayTo = endTime
+            this.index = -1
+            this.onSearch()
+        }
+
+    }
+
+    this.onChangeTimeFormat = function (timestamp) {
+        var date = new Date(timestamp * 1000);
+        return date.format("yyyy-MM-dd hh:mm:ss");
+    }
+})
