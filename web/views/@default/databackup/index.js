8d54532e9706aef77f8f5e231a47fabceef0c504
hanchan <1256668725@qq.com>
2021-08-01 00:59:11 +0800

    更新nextcloud代码

--- web/views/@default/databackup/index.js
+++ web/views/@default/databackup/index.js
@@ -1,5 +1,5 @@
 Tea.context(function () {
-
+    this.success = NotifyReloadSuccess("保存成功")
     this.pageState = 1
 
     this.fileDesc = ""
@@ -19,8 +19,14 @@ Tea.context(function () {
         return resultTime;
       };
 
-    this.onDownFile = function (id) {
-        
+    this.onDownFile = function (name) {
+        teaweb.confirm("确定下载该文件？",function() {
+            this.$get("/databackup/download").params({
+                name: name,
+            }).success((res)=>{
+                this.onDownLoadLocalFile(res.data.url,res.data.token)
+            })
+        })
     }
 
     this.onEdit = function (id) {
@@ -33,9 +39,52 @@ Tea.context(function () {
 		})
     }
 
-    this.onDelete = function (id) {
-        
+    this.onDelete = function (name) {
+        teaweb.confirm("确定要删除该文件？",function() {
+            this.$post("/databackup/delete").params({
+                Opt: "delete",
+                name: name,
+            }).refresh()
+        })
     }
+
+    this.onDownLoadLocalFile=function(downUrl,headToken){
+        function sucHandle(res){
+            console.log('sucHandle')
+            console.log(res)
+        }
+        function failHandle(){
+            console.log('failHandle')
+        }
+        Tea.openDownloadUrl("post",downUrl,headToken,sucHandle,failHandle)
+        // let xhr = new XMLHttpRequest()
+        // xhr.open("post",downUrl,true)
+        // xhr.setRequestHeader("Authorization","Bearer "+headToken)
+        // xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+        // xhr.setRequestHeader("Access-Control-Allow-Origin","*")
+        // xhr.setRequestHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept ")
+        // xhr.send()
+        // xhr.responseType="blob"
+        // xhr.onload=function(){
+        //     if(this.status==200){
+        //         let tempBlob = this.response
+        //         let tempReader = new FileReader()
+        //         tempReader.onload=function(){
+        //             let link = document.createElement("a")
+        //             link.href=URL.createObjectURL(tempBlob)
+        //             link.setAttribute("download","text.pdf")
+        //             link.click()
+        //             link=null
+        //         }
+        //     }
+        // }
+    }
+
+    // this.onuploadFile = function (file) {
+    //     this.$post("/databackup").params({
+    //         uploadFile: file
+    //     }).success()
+    // }
 
     this.tableData = [
         {id:1,value1:"数据库agent.exe",value2:"CloudShield用户手册V1.0",value3:"100K",value4:"2021-06-30T21:52:20.123"}
