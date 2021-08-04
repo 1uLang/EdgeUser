630422ba02509a1af8a51ea9ec6e99383db1cbd1
1uLang <243971996@qq.com>
2021-08-05 02:32:50 +0800

    主机防护bug

--- internal/web/actions/default/hids/agent/install.go
+++ internal/web/actions/default/hids/agent/install.go
@@ -44,7 +44,7 @@ func (this *InstallAction) RunGet(params struct {
 	if url == "" {
 		url = "https://hids.zhiannet.com"
 	}
-	linux = strings.Replace(linux, "https://172.18.200.29", "https://user.cloudhids.net", 1)
+	linux = strings.Replace(linux, "https://172.18.200.29", url, 1)
 	linux = strings.ReplaceAll(linux, "172.18.200.29", "156.240.95.243")
 
 	windows = strings.Replace(windows, "172.18.200.29", "156.240.95.243", 1)
