c6c1bd7d59396f02e614e40a8879d838e4b5e87c
1uLang <243971996@qq.com>
2021-08-06 20:56:23 +0800

    堡垒机后端测试

--- internal/web/actions/default/fortcloud/admins/index.go
+++ internal/web/actions/default/fortcloud/admins/index.go
@@ -1,8 +1,6 @@
 package admins
 
 import (
-	"fmt"
-	admin_users_model "github.com/1uLang/zhiannet-api/jumpserver/model/admin_users"
 	jumpserver_server "github.com/1uLang/zhiannet-api/jumpserver/server"
 	"github.com/TeaOSLab/EdgeUser/internal/web/actions/actionutils"
 	"github.com/TeaOSLab/EdgeUser/internal/web/actions/default/fortcloud"
@@ -29,17 +27,17 @@ func (this *IndexAction) Init() {
 
 func (this *IndexAction) RunGet() {
 
-	req, err := this.checkAndNewServerRequest()
-	if err != nil {
-		this.Data["nodeErr"] = "堡垒机组件错误:" + err.Error()
-		this.Show()
-		return
-	}
-
-	list, err := req.AdminUser.List(&admin_users_model.ListReq{
-		UserId: uint64(this.UserId()),
-	})
-	this.Data["adminUsers"] = list
+	//req, err := this.checkAndNewServerRequest()
+	//if err != nil {
+	//	this.Data["nodeErr"] = "堡垒机组件错误:" + err.Error()
+	//	this.Show()
+	//	return
+	//}
+	//
+	//list, err := req.AdminUser.List(&admin_users_model.ListReq{
+	//	UserId: uint64(this.UserId()),
+	//})
+	//this.Data["adminUsers"] = list
 	this.Show()
 }
 
@@ -52,36 +50,36 @@ func (this *IndexAction) RunPost(params struct {
 	Must *actions.Must
 }) {
 
-	params.Must.
-		Field("name", params.Name).
-		Require("请输入名称")
-
-	params.Must.
-		Field("username", params.Username).
-		Require("请输入用户名")
-
-	params.Must.
-		Field("password", params.Password).
-		Require("请输入密码")
-
-	req, err := this.checkAndNewServerRequest()
-	if err != nil {
-		this.ErrorPage(fmt.Errorf("堡垒机组件错误:" + err.Error()))
-		return
-	}
-
-	args := &admin_users_model.CreateReq{}
-	args.Name = params.Name
-	args.UserName = params.Username
-	args.Password = params.Password
-	args.Comment = params.Comment
-	args.UserId = uint64(this.UserId())
-	_, err = req.AdminUser.Create(args)
-	if err != nil {
-		this.ErrorPage(err)
-		return
-	}
-	// 日志
-	this.CreateLogInfo("堡垒机 - 新增管理用户:[%v]成功", params.Name)
+	//params.Must.
+	//	Field("name", params.Name).
+	//	Require("请输入名称")
+	//
+	//params.Must.
+	//	Field("username", params.Username).
+	//	Require("请输入用户名")
+	//
+	//params.Must.
+	//	Field("password", params.Password).
+	//	Require("请输入密码")
+	//
+	//req, err := this.checkAndNewServerRequest()
+	//if err != nil {
+	//	this.ErrorPage(fmt.Errorf("堡垒机组件错误:" + err.Error()))
+	//	return
+	//}
+	//
+	//args := &admin_users_model.CreateReq{}
+	//args.Name = params.Name
+	//args.UserName = params.Username
+	//args.Password = params.Password
+	//args.Comment = params.Comment
+	//args.UserId = uint64(this.UserId())
+	//_, err = req.AdminUser.Create(args)
+	//if err != nil {
+	//	this.ErrorPage(err)
+	//	return
+	//}
+	//// 日志
+	//this.CreateLogInfo("堡垒机 - 新增管理用户:[%v]成功", params.Name)
 	this.Success()
 }
