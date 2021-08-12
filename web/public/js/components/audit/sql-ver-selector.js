48d5d3c320983a5ff13997184dc857e27d3ffcd0
dengpang <404821634@qq.com>
2021-08-12 14:07:15 +0800

    优化

--- web/public/js/components/audit/sql-ver-selector.js
+++ web/public/js/components/audit/sql-ver-selector.js
@@ -15,28 +15,67 @@ Vue.component("sql-ver-selector", {
             assemblyVer = ""
         }
         let assemblyType = this.vAssemblyType
+        // console.log(assemblyType)
         if (assemblyType == null) {
             assemblyType = -1
         }
         let isEdit = this.vAssemblyEdit
-        let assemblys = [
-            {"name": "5.5"},
-            {"name": "5.6"},
-            {"name": "5.7"},
-            {"name": "8.0"},
-        ]
-        if (assemblyType == 2) {
-            assemblys = [
-                {"name": "2005"},
-                {"name": "2008"},
-                {"name": "2012"},
-                {"name": "2014"},
-                {"name": "2016"},
-                {"name": "2017"},
-                {"name": "2019"},
-            ]
-        }
 
+        let assembly0 = [
+            {"name":"5.1"},{"name":"5.2"},{"name":"5.3"},{"name":"5.5"},{"name":"10.0"},
+            {"name":"10.1"},{"name":"10.2"}, {"name":"10.3"}, {"name":"10.4"}, {"name":"10.5"},{"name":"10.6"},
+        ]
+        let assembly1 = [
+            {"name":"5.5"},
+            {"name":"5.6"},
+            {"name":"5.7"},
+            {"name":"8.0"},
+            {"name":"8.1"},
+        ]
+        let assembly2 = [
+            {"name":"2005"},
+            {"name":"2008"},
+            {"name":"2012"},
+            {"name":"2014"},
+            {"name":"2016"},
+            {"name":"2017"},
+            {"name":"2019"},
+        ]
+        let assembly3 = [
+            {"name":"9.5"},
+            {"name":"9.6"},
+            {"name":"10"},
+            {"name":"11"},
+            {"name":"12"},
+            {"name":"13"},
+            {"name":"14"},
+        ]
+        let assembly4 = [
+            {"name":"2.1"},
+            {"name":"2.4"},
+            {"name":"3.0"},
+            {"name":"4.0"},
+            {"name":"4.2"},
+        ]
+        switch(assemblyType){
+            case "0":
+                assemblys = assembly0
+                break;
+            case "1":
+                assemblys = assembly1
+                break;
+            case "2":
+                assemblys = assembly2
+                break;
+            case "3":
+                assemblys = assembly3
+                break;
+            case "4":
+                assemblys = assembly4
+                break;
+            default:
+                assemblys = assembly1
+        }
 		return {
 			assemblys: assemblys,
 			assemblyVer: assemblyVer,
@@ -45,26 +84,62 @@ Vue.component("sql-ver-selector", {
 	},
 	methods:{
       onResetData(){
-       let assembly1 = [
-        {"name":"5.5"},
-        {"name":"5.6"},
-        {"name":"5.7"},
-        {"name":"8.0"},
-       ]
-       let assembly2 = [
-        {"name":"2005"},
-        {"name":"2008"},
-        {"name":"2012"},
-        {"name":"2014"},
-        {"name":"2016"},
-        {"name":"2017"},
-        {"name":"2019"},
-       ]
-       this.assemblys = assembly1
-       if(this.assemblyType == 2){
-        this.assemblys = assembly2
-       }
-       this.assemblyVer = ""
+          let assembly0 = [
+              {"name":"5.1"},{"name":"5.2"},{"name":"5.3"},{"name":"5.5"},{"name":"10.0"},
+              {"name":"10.1"},{"name":"10.2"}, {"name":"10.3"}, {"name":"10.4"}, {"name":"10.5"},{"name":"10.6"},
+          ]
+          let assembly1 = [
+              {"name":"5.5"},
+              {"name":"5.6"},
+              {"name":"5.7"},
+              {"name":"8.0"},
+              {"name":"8.1"},
+          ]
+          let assembly2 = [
+              {"name":"2005"},
+              {"name":"2008"},
+              {"name":"2012"},
+              {"name":"2014"},
+              {"name":"2016"},
+              {"name":"2017"},
+              {"name":"2019"},
+          ]
+          let assembly3 = [
+              {"name":"9.5"},
+              {"name":"9.6"},
+              {"name":"10"},
+              {"name":"11"},
+              {"name":"12"},
+              {"name":"13"},
+              {"name":"14"},
+          ]
+          let assembly4 = [
+              {"name":"2.1"},
+              {"name":"2.4"},
+              {"name":"3.0"},
+              {"name":"4.0"},
+              {"name":"4.2"},
+          ]
+          this.assemblys = assembly1
+
+          switch(this.assemblyType){
+              case "0":
+                  this.assemblys = assembly0
+                  break;
+              case "1":
+                  this.assemblys = assembly1
+                  break;
+              case "2":
+                  this.assemblys = assembly2
+                  break;
+              case "3":
+                  this.assemblys = assembly3
+                  break;
+              case "4":
+                  this.assemblys = assembly4
+                  break;
+          }
+          this.assemblyVer = ""
       }
     },
 	watch:{
