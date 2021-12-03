605535a5e6ecdbafb73d5499e480c293e707960c
1uLang <243971996@qq.com>
2021-12-03 21:20:15 +0800

    创建子账号页面 bug

--- web/views/@default/platform/user/create.js
+++ web/views/@default/platform/user/create.js
@@ -9,4 +9,24 @@ Tea.context(function () {
             this.otpIsOn = false
         }
     }
+    this.submitBefore = function () {
+        //获取表单提交按钮
+        var btnSubmit = document.getElementById("submit-btn");
+        //将表单提交按钮设置为不可用，可以避免用户再次点击提交按钮进行提交
+        btnSubmit.disabled = "disabled";
+    }
+    this.submitFail = function (resp) {
+
+        var error = resp.errors[0].messages[0]
+        teaweb.warn(error)
+        //获取表单提交按钮
+        var btnSubmit = document.getElementById("submit-btn");
+        //将表单提交按钮设置为不可用，可以避免用户再次点击提交按钮进行提交
+        btnSubmit.disabled = false;
+    }
+    this.submitError = function (resp) {
+        var btnSubmit = document.getElementById("submit-btn");
+        //将表单提交按钮设置为不可用，可以避免用户再次点击提交按钮进行提交
+        btnSubmit.disabled = false;
+    }
 })
