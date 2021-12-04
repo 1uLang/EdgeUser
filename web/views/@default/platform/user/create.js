04eaa398695f49635ecf341e892a8cbacf8f731f
1uLang <243971996@qq.com>
2021-12-04 10:21:56 +0800

    创建子账号页面 bug

--- web/views/@default/platform/user/create.js
+++ web/views/@default/platform/user/create.js
@@ -20,9 +20,10 @@ Tea.context(function () {
         if (resp.message !== "") {
             teaweb.warn(resp.message)
             return
+        }else{
+            var error = resp.errors[0].messages[0]
+            teaweb.warn(error)
         }
-        var error = resp.errors[0].messages[0]
-        teaweb.warn(error)
         //获取表单提交按钮
         var btnSubmit = document.getElementById("submit-btn");
         //将表单提交按钮设置为不可用，可以避免用户再次点击提交按钮进行提交
